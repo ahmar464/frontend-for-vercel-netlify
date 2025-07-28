e8ec58770e51e337c83044b061cb4f71f5e77614
ahmar464 <sheikhahamr464@gmail.com>
2025-07-28 22:18:23 +0500

    basic react app

---
+++ my-react-demo/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      <p>Deployed on Vercel & Netlify 🚀</p>
+    </div>
+  );
+}
+
+export default App;
